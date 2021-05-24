cf7453c3e69b0bb0e28972049920c48b11d040ff
Vianka213 <u18278273@tuks.co.za>
2021-05-24 07:56:48 +0200

    Added user and role paths

--- wholesome-server/routes/index.router.js
+++ wholesome-server/routes/index.router.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const asyncify = require('express-asyncify')
 const router = asyncify(express.Router());
+const user = require('../controllers/user.controller');
+const role = require('../controllers/role.controller');
+const userHelper = require('../helpers/user.helper');
+
+//user
+router.post("/user/register", user.register);
+router.post("/user/login", user.login);
+router.get("/user/getRoles",jwtHelper.verifyJWTtoken, user.getRoles);
+router.get("/user/getName",jwtHelper.verifyJWTtoken, user.getName);
+router.get("/user/isVerified",jwtHelper.verifyJWTtoken, user.isVerified);
+router.post("/user/verifyUser",jwtHelper.verifyJWTtoken, userHelper.isAdmin, user.verify);
+
+//role
+router.get("/role/getRole", jwtHelper.verifyJWTtoken, role.getRole);
 
 module.exports = router;
