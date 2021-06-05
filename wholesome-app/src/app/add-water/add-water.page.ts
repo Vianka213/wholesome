55adadbe5a335c18e0f302ea0d3f1a032c040c4a
Vianka213 <u18278273@tuks.co.za>
2021-06-05 19:51:45 +0200

    Add water

--- wholesome-app/src/app/add-water/add-water.page.ts
+++ wholesome-app/src/app/add-water/add-water.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-add-water',
@@ -10,11 +11,34 @@ export class AddWaterPage implements OnInit {
 
   constructor(public viewCtrl: ModalController) { }
 
+  private chart : Chart
+  amountWater : 2
+  maxWater : 8
+
   ngOnInit() {
+    this.createChart()
   }
 
   dismissModal() {
     this.viewCtrl.dismiss();
+  }
+
+  createChart() {
+    this.chart = new Chart('waterCanvas', {
+      type: "doughnut",
+      data: {
+        datasets: [
+          {
+            data: [this.amountWater, this.maxWater],
+            backgroundColor: [
+              "#439BFF",
+              "#F5F6FA",
+            ],
+            hoverBackgroundColor: ["#439BFF", "#F5F6FA"]
+          }
+        ]
+      }
+    });
   }
 
 }
