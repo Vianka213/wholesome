3b558382d491518cde65a7b65a8f36d5b74f937f
Vianka213 <u18278273@tuks.co.za>
2021-06-17 15:57:38 +0200

    Exercise page and search

--- wholesome-app/src/app/add-exercise/add-exercise.page.ts
+++ wholesome-app/src/app/add-exercise/add-exercise.page.ts
@@ -10,7 +10,40 @@ export class AddExercisePage implements OnInit {
 
   constructor(public viewCtrl: ModalController) { }
 
+  test = {
+    "exercises": [
+      {
+        "tag_id": 316,
+        "user_input": "ran",
+        "duration_min": 34.63,
+        "met": 9,
+        "nf_calories": 363.62,
+        "photo": {
+          "thumb": "https://d2xdmhkmkbyw75.cloudfront.net/exercise/316_thumb.jpg",
+          "highres": null
+        },
+        "compendium_code": 12040,
+        "name": "running"
+      }
+    ]
+  }
+
+  searchQuery : String = ''
+  searchResults : Object[] = []
+  addedExercise : Object[] = []
+  segment : string = "all"
+
   ngOnInit() {
+  }
+
+  searchExercise(query) {
+    console.log(query)
+    /*this.trackerService.naturalSearchEx(query).subscribe( data => {
+      this.searchResults = data['exercises']
+      console.log(this.searchResults)
+    })*/
+
+    this.searchResults = this.test['exercises']
   }
 
   dismissModal() {
